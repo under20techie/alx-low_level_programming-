089a7c27149c5f541ebdc9c627b46644f4f2e075
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-09 04:09:19 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,12 @@
 /**
  * main - Entry point
  *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
