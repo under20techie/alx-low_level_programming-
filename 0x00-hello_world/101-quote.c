a3b3ba2cd6d3c0359ae7a865347ea23a513581bf
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-09 23:30:46 +0100

    Now

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <unistd.h>
+#include <stdio.h> 
+#include <unistd.h> 
 
 /**
  * main - print quote 
@@ -11,8 +11,7 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - 
-        Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
