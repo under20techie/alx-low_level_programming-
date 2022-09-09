c3ebbb87d964cc7bdc01fc14d3b1a9e27692e946
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-09 15:16:56 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,28 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
-<<<<<<< HEAD
- *  * main - print quote
- *   *
- *    * Description: prints Dora's quote
- *     *
- *      * Return: Always 1 (Success)
- *       */
+ * main - print quote
+ *
+ * Description: prints Dora's quote
+ *
+ * Return: Always 1 (Success)
+ */
 
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
-=======
-* main - print quote
-*
-* Description: prints Dora's quote
-*
-* Return: Always 1 (Success)
-*/
-int main(void)
-{
-   write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-   return (1);
->>>>>>> 187c0979b028d12f790fae8a5f2256588acf7e7c
-}
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
