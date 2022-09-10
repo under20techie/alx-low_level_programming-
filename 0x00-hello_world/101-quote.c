ea5502debac712676d21302d6450ee7d1589d352
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-10 03:12:30 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
-=======
-#include <stdio.h> 
-#include <unistd.h> 
 
->>>>>>> a3b3ba2cd6d3c0359ae7a865347ea23a513581bf
 /**
- * main - print quote 
+ * main - print quote
  *
- * Description:print dora quote
+ * Description: prints Dora's quote
  *
  * Return: Always 1 (Success)
  */
@@ -18,6 +12,5 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
 	return (1);
 }
