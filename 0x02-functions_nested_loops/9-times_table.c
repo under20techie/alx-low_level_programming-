8428f3542f7eb6589e7a6cbee0982d6620f0b8d5
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-13 22:13:46 +0100

    Create 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  int i;
+  int n = 9;
+	for (i = 1; i <= 10; ++i)
+{
+    putchar("%d * %d = %d \n", n, i, n * i)
+}
+	return (0);
+}
