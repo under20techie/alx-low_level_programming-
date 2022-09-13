57e31866874cf103273520d790573dc4afb96c2b
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-13 21:41:24 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,9 +13,9 @@ void jack_bauer(void)
 	int hours = 0;
 	int minutes = 0;
 
-	while(hours <= 23)
+	while (hours <= 23)
 {
-		while(minutes <= 59)
+		while (minutes <= 59)
 {
 			printf("%02d:%02d\n", hours, minutes);
 			minutes++;
