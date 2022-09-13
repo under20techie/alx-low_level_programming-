679b4a3e304e6a9f7571352e0fa4217a13bb771e
agiamahmary <maryagiamah@gmail.com>
2022-09-13 06:04:00 -0700

    sleep

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description : printing _putchar
+ *
+ * Return - Always 0 (Success)
+ */
+
+int main()
+{	char school[4] = "NNSS";
+	int n = 0;
+	
+	for(n = 0; n < 4; n++;){
+	 _putchar(school[n];
+}
+	 _putchar("\n");
+	 return(0);
+}
