6fb500d1b52194662814d61a52d800d04e0112e8
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-13 15:02:40 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,11 +11,11 @@
 int main(void)
 {	char school[9] = "_putchar";
 	int n = 0;
-
-for (n = 0; n < 8; n++)
+	
+ 	for (n = 0; n < 8; n++)
 	{
-	 _putchar(school[n]);
+		 _putchar(school[n]);
 	}
-	 _putchar('\n');
-	 return (0);
+	 	_putchar('\n');
+		 return (0);
 }
