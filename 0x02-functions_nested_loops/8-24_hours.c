34cab342557d2f4d40dccbe3884884cb12fcb6b9
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-13 21:29:53 +0100

    Create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day.
+ *
+ * Description: prints every minute of the day.
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int hours = 0;
+  int minutes = 0;
+  
+  while(hours <= 23){
+  while(minutes <= 59){
+  printf("%02d:%02d\n", hours, minutes);
+  minutes++;
+}
+hours++;
+  minutes = 0
+	}
+}
