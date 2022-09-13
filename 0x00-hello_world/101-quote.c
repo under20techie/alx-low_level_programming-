3d02e1b6d629fe9d635d6ab8b63607475741f80a
agiamahmary <maryagiamah@gmail.com>
2022-09-12 21:01:49 -0700

    sleep

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include <stdio.h>
 #include <unistd.h>
 
@@ -15,21 +15,3 @@ int main(void)
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-=======
-#include <stdio.h>
-#include <unistd.h>
-
-/**
- * main - print quote
- *
- * Description: prints Dora's quote
- *
- * Return: Always 1 (Success)
- */
-
-int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}
->>>>>>> 53dce924a6be2f15bb944ed01b8af593d5a0ef47
