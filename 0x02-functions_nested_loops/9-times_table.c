42cb0215e43336a481851795159e5bd101512641
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-13 22:37:13 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,23 +1,44 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * times_table - prints the 9 times table
  *
- * Description: Creating 9 timestable.
+ * Description: prints the 9 times table
  *
- * Return: Always 0.
+ * Return: void
  */
-void times_table(int product)
+
+void times_table(void)
 {
-  int i = 0;
-  int n = 9;
-	int product;
-	product = i * n;
-	for (i = 0; i <= 12; ++i)
-{
-   
-		++i;
-}
-	return (product);
+	int row, column, product, tens, ones;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
+			tens = product / 10;
+			ones = product % 10;
+
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
