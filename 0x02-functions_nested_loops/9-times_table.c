0f0824269f19e78fe3c97fcb6d63915d1377c8f7
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-13 22:30:43 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,12 +8,12 @@
  *
  * Return: Always 0.
  */
-void times_table(int j)
+void times_table(void)
 {
   int i;
   int n = 9;
-	int j;
-	j = i * n
+	int product;
+	product = i * n
 	for (i = 1; i <= 12; ++i)
 {
    
