2c9c79f6959617032901fde1055c77fedc44230b
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-15 20:24:07 +0100

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * print_line - prints a line of \ n long
+ * print_diagonal - prints a line of \ n long
  * @n: length of line
  *
  * Return: void
  */
 
-void print_line(int n)
+void print_diagonal(int n)
 {
 	int line = 0;
 
 	while (line < n)
 	{
-		_putchar('\');
+		_putchar('\\');
 		line++;
 	}
 	_putchar('\n');
