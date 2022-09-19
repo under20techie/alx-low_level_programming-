3e412b511f73fe644887f8907092e752810a6c2c
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-19 20:32:23 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,14 +3,18 @@
 /**
 * _strlen - Reads the length of string 
 *
-* @c - Returns the value of a string
+* @*s - Returns the value of a string
 *
 *Return - l;
 */
 
 int _strlen(char *s)
 {
-int length;
-        for (k = 0; ,k < length; k++)
-{
+int length = 0;
+int k;
+	for (k = 0; ,k < length; k++)
+	{
+		length += s[k];
+	}
+	return (length);
 
