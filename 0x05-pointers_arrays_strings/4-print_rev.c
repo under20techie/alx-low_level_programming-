0e79cbf5b5b098486c2bca4fe7cc671fd72da416
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-19 21:38:17 +0100

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_rev - prints the reverse of  a string.
+* @s: a pointer to the string array.
+*
+* Return: Always0 (Success)
+*/
+
+void print_rev(char *s)
+{
+  int r = 0;
+  
+  while (s[r])
+  {
+    printf("%c", s[r]);
+    r--;
+  }
+  printf("\n");
+}
