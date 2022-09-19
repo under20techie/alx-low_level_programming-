9dfd957e5c1ad475b42186eb8a1e4b362508ec5f
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-19 20:15:39 +0100

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,16 @@
 #include "main.h"
+
+/**
+* _strlen - Reads the length of string 
+*
+* @c - Returns the value of a string
+*
+*Return - l;
+*/
+
+int _strlen(char *s)
+{
+int length;
+        for (k = 0; ,k < length; k++)
+{
+
