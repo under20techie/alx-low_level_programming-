acbe1f088099876c9527955e3b322774073b8531
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-19 19:36:47 +0100

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* swap_int(int *a, int *b) - swap values of *a and *b.
+*
+* @a,b:swap both the value of pointers a and b.
+*
+* Return: Always 0 (Success);
+*/
+
+void swap_int(int *a, int *b)
+{
+int k;
+K = *a;
+*a = *b;
+*b = k;
+}
