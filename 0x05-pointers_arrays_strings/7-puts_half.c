a4bbd809b5842c5f1e8210c0965de73aa2f563e7
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-20 01:08:53 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,20 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+* puts_half - prints the reverse of  a string.
+* @str: a pointer to the string array.
+*
+* Return: Always0 (Success)
+*/
+
+void puts_half(char *str)
+{
+	int v = 0;
+
+	while (str[v])
+		printf("%c", str[v]);
+	r = r / 2
+	}
+	printf("\n");
+}
