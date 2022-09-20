9e8ff0769e54f6037c671fef38d51283e6c7f781
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-20 01:58:00 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,12 +12,15 @@ void puts_half(char *str)
 
 int z, l;
 l = strlen(str);
-	for (z = 0; z < l; z++)
+	for (z = 0; z < l; z--)
 	{
 		printf("%c", str[z]);
 		z = z / 2;
-	}
-		z = z % 2;
+	 while (str[z])
+	 {
 		printf("%c" , str[z]);
+		z = z % 2;
+		 z--;
+	 }
 	printf("\n");
 }
