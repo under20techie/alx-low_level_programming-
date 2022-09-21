56255fa29c1ad8afce2c2532a4bda5bde84f9074
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-22 00:20:37 +0100

    Create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include "main.h"
+
+/**
+* _strcat - a  function that concatenates two strings char to dest.
+* char *src: pointer to string being appended to dest string.
+* char *dest: a pointer to string that scr is being added to.
+* Return: Always 0 (Success);
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int k , l , m, n;
+	int l = strlen (dest - 1);
+	int n = strlen (scr);
+	for (k = 0; k < l; k++)
+	{
+		for (m = 0; m <= n; m++)
+			strcat (dest[k], scr[]);
+	}
+	return dest[k];
+}
