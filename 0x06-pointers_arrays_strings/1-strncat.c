ada9bd4de33fc56c54a72fec544dd92d028b1552
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-22 02:11:09 +0100

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strncat - appends src to the dest string
+ * @dest: string to append by src
+ * @src: string to append to dest
+ * @n: largest number of bytes to append
+ *
+ * Return: address of dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int l, p;
+
+	l = j = 0;
+	while (*(dest + l))
+		i++;
+	if (j < n)
+		*(dest + i) = *(src + j);
+	return (dest);
+}
