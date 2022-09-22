8252dbcc9c4a88366bbe28cc755458f940d0d042
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-09-22 01:56:15 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,15 +11,15 @@
 char *_strcat(char *dest, char *src)
 {
 	int k , m;
+	
 	k = m = 0;
 	
 	while ((*(dest + k)))
 		 k++;
-	       
 	while (((*(dest + k) = *(src + m))))
-		 {
+{
 		m++;
 		k++;
-		 }
-return (dest);		
+}
+return (dest);	
 }
