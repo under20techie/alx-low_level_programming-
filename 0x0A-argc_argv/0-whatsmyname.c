e6e3370bf6eebfd17d1f7a1903e81336887ddb21
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-10-01 01:17:52 +0100

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* printsargv - function that prints itslef followed by a new name
+* @argc: parameter that counts the number of arguements passed to the function on the command line
+* @argv:an array poniter to the number of strings
+* Return: 0 always sucess
+*/
+int printsargv(argc__attribute__((unused)) , char *argv[])
+{
+	printf("%s\n", argv[0]);
+return (0);
+}
