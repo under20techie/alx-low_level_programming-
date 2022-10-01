e3e99845057c941c9d9ccfb13f2da47d62ae2d81
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-10-01 02:21:50 +0100

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - function that prints itslef followed by a new name
+* @argc: parameter that counts the number of arguements passed to the function
+* @argv:an array poniter to the number of strings
+* Return: 0 always sucess
+*/
+int main(int argc, char *argv[])
+{
+int i;
+int mult;
+  if (i < 2)
+{
+    printf("%s\n", Error)
+  return (1);
+}
+	for (i = 1; i < argc; i++)
+    mult *= atoi(argv[i]);
+		printf("%d\n", argv[i]);
+	return (0);
+}
