2314b6a6a8b9cc6c2d09c30ad6add75778f2a7ff
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-10-01 01:24:44 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
 * @argv:an array poniter to the number of strings
 * Return: 0 always sucess
 */
-int printsargv(int argc__attribute__((unused)) , char *argv[])
+int printsargv(int argc __attribute__((unused)) , char *argv[])
 {
 	printf("%s\n", argv[0]);
 return (0);
