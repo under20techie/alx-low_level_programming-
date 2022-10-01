fb5e334d59b0b81ebd8b5026ab9578358a3273b0
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-10-01 03:07:26 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,27 +1,25 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
-* main - function that prints itslef followed by a new name
-* @argc: parameter that counts the number of arguements passed to the function
-* @argv:an array poniter to the number of strings
-* Return: 0 always sucess
-*/
+ * main - multiplies two numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: 0 if no errors, else 1
+ */
 int main(int argc, char *argv[])
 {
-int i;
-int mult = 1;
-if (argc < 2)
-{
-	printf("Error");
-	return (1);
-}
-for (i = 1; i < argc; i++)
-{
-	if( i > 2)
-	break;
-	mult = mult * atoi(argv[i]);
-	printf("%d\n", mult);
-}
-return (0);
+	int a, b, c;
+
+	if (argc != 3)
+	{
+		puts("Error");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	c = a * b;
+	printf("%d\n", c);
+	return (0);
 }
