0a3ae41dc9978495182493fdeb33abbefa6bc6ef
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-10-01 03:06:22 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 {
 int i;
 int mult = 1;
-if (argc < 3)
+if (argc < 2)
 {
 	printf("Error");
 	return (1);
@@ -19,7 +19,7 @@ if (argc < 3)
 for (i = 1; i < argc; i++)
 {
 	if( i > 2)
-		break;
+	break;
 	mult = mult * atoi(argv[i]);
 	printf("%d\n", mult);
 }
