f7e9afdc31f9f078741042ba20aeb9d4439942a7
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-10-01 01:57:02 +0100

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - function that prints itslef followed by a new name
+* @argc: parameter that counts the number of arguements passed to the function
+* @argv:an array poniter to the number of strings
+* Return: 0 always sucess
+*/
+int main(int argc, char *argv[])
+{
+int i;
+  for (i = 0; i < argc; i++)
+	  printf("%s\n", argv[i]);
+return (0);
+}
