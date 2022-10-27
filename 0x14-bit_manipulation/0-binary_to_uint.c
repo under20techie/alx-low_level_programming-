64843c27816d150ac6f340a6785c8f599a958948
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2022-10-28 00:08:11 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,16 +12,16 @@ int i;
 unsigned int num;
 num = 0;	
 if (!b)		
-return (0);	
+        return (0);	
 for (i = 0; b[i] != '\0'; i++)
 {
-if (b[i] != '0' && b[i] != '1')
+        if (b[i] != '0' && b[i] != '1')
         return (0);
 }	
 for (i = 0; b[i] != '\0'; i++)
 {
-num <<= 1;
-if (b[i] == '1')
+        num <<= 1;
+        if (b[i] == '1')
         num += 1;
 }	
 return (num);
