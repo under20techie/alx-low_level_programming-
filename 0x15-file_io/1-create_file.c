03669a66dae2da1c97a9b15e46d1e53e11650b7a
under20techie <133390683+under20techie@users.noreply.github.com>
2023-10-31 23:46:47 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,3 +33,19 @@ int create_file(const char *filename, char *text_content)
 
 	return (1);
 }
+
+/**
+* my_strlen - Reads the length of string
+*
+* @s: Returns the value of a string
+*
+* Return: k;
+*/
+
+int my_strlen(char *s)
+{
+	int k = 0;
+	while (s[k])
+		k++;
+	return (k);
+}
