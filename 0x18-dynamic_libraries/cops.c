1d338d1a4807b470ae5d4e7c5f356181a6b189e0
Under20techie <under20techie@gmail.com>
2023-12-12 10:54:16 +0000

    you

---
+++ 0x18-dynamic_libraries/cops.c
@@ -0,0 +1,21 @@
+#include "cops.h"
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
